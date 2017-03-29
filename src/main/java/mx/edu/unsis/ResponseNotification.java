f46d3c6ac690775a79be4ef1d04cdaa32f97f5de
alfredo <benitoalfredoreyes@gmail.com>
2017-03-28 21:35:30 -0600

    Request y Response de Notificaciones

---
+++ src/main/java/mx/edu/unsis/ResponseNotification.java
@@ -0,0 +1,28 @@
+package mx.edu.unsis;
+
+public class ResponseNotification {
+	private boolean statuscon;
+	private boolean procstatus;
+	private int totalenviados;
+	
+	public boolean getStatuscon() {
+		return statuscon;
+	}
+	public void setStatuscon(boolean statuscon) {
+		this.statuscon = statuscon;
+	}
+	public boolean getProcstatus() {
+		return procstatus;
+	}
+	public void setProcstatus(boolean procstatus) {
+		this.procstatus = procstatus;
+	}
+	public int getTotalenviados() {
+		return totalenviados;
+	}
+	public void setTotalenviados(int totalenviados) {
+		this.totalenviados = totalenviados;
+	}
+	
+	
+}
