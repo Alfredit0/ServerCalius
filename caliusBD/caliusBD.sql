803a0a7d0f814a508dc11a870e028ed6c7693e65
alfredo <benitoalfredoreyes@gmail.com>
2017-03-28 22:43:54 -0600

    Sentencias Insert En Base de datos Sql

--- caliusBD/caliusBD.sql
+++ caliusBD/caliusBD.sql
@@ -74,3 +74,14 @@ primary key (usuarioId));
 alter table notificaciones add foreign key (notifRemitente) references  usuarios(usuarioId) on delete cascade;
 alter table calificaciones add foreign key (cAlumnoMatricula) references  alumnos(alumnoMatricula) on delete cascade;
 alter table calificaciones add foreign key (cMateriaId) references  materias(materiaId) on delete cascade;
+
+
+-- Alta Alumnos
+insert into alumnos values("2013060024", "Benito Alfredo", "Reyes", "Hernandez", "LCI", "806", 8, "2017-A");
+insert into alumnos values("2013060012", "Minerva", "Martinez", "Rios", "LCI", "806", 8, "2017-A");
+
+-- Alta Administrativos
+insert into Administrativos values("3256", "Gabriela Garcia", "Jefa Servicios Escolares");
+
+-- Alta usuario
+insert into usuarios values("2013060024", "9581168329", "APA91bFIS5FRUlV4ahhq2nB5DCJpoCClAJBQJ_cDwOwO0RAx-PHDxsQ2PrZwhHIej-8uhBjP-Rx5g2fzRY9qTUn9J2aQQkJ2AWslbRliDeu4zPBxmMaEXReRvaVdCXumctYH4AQsZ8IeEr_LezrxNjYptI9JYD4MeA", "caliuspass");
